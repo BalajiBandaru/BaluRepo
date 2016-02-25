b28ed7db7c8ab918c22dc27306e05df00301da6e
keystrat <keystrat@keystrat-PC>
2016-02-25 15:38:50 -0600

    -----1-----

---
+++ HelloMaven/src/test/java/com/org/helloWorld/HelloMaven/HelloMavenTest.java
@@ -0,0 +1,13 @@
+package com.org.helloWorld.HelloMaven;
+
+import org.testng.annotations.Test;
+
+public class HelloMavenTest {
+
+@Test
+public void login()
+{
+	System.out.println("Hello World");
+}
+
+}
